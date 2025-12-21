495f08ee30fcd5d9a265ef46f430f845ffe40167
kladov-vv <kladov.vv@yandex.com>
2025-12-21 23:23:10 +1000

    Add function for displaying library

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
-const myLibrary = [];
+const libraryBoard = document.querySelector('.library-board');
+const myLibrary = [
+    {title: 'book 1', author: 'author 1', pages: 100, id: 1},
+    {title: 'book 2', author: 'author 2', pages: 200, id: 2}
+];
 
 function Book(title, author, pages) {
   this.title = title;
@@ -9,4 +13,14 @@ function Book(title, author, pages) {
 
 function addBookToLibrary(title, author, pages) {
     myLibrary.push(new Book(title, author, pages));
+}
+
+function displayLibrary() {
+    libraryBoard.replaceChildren();
+    for (const book of myLibrary) {
+            let div = document.createElement("div");
+            div.classList.add("book-card");
+            div.textContent = `${book.title} by ${book.author} (${book.pages} pages), ID: ${book.id}`;
+            libraryBoard.appendChild(div);
+    }   
 }
