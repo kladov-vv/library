3b9f63f3ae367b7fdd09af3eb47943afcf42ce09
kladov-vv <kladov.vv@yandex.com>
2025-12-27 22:56:51 +1000

    Add ability to remove book from library board

--- main.js
+++ main.js
@@ -1,26 +1,25 @@
-const libraryBoard = document.querySelector('.library-board');
 const dialog = document.querySelector('dialog');
 const form = document.querySelector('form');
-const btnAdd = document.querySelector('.btn-add');
-const btnClose = document.querySelector('.btn-close');
+const btnCloseDialog = document.querySelector('.btn-close-dialog');
+const btnAddBook = document.querySelector('.btn-add-book');
+const libraryBoard = document.querySelector('.library-board');
+
 const myLibrary = [
     {title: 'book 1', author: 'author 1', pages: 100, id: 1},
     {title: 'book 2', author: 'author 2', pages: 200, id: 2}
 ];
 
-btnAdd.addEventListener('click', () => {
+// EVENTS
+btnAddBook.addEventListener('click', () => {
     dialog.showModal();
 });
 
-btnClose.addEventListener('click', (event) => {
-    event.preventDefault();
+btnCloseDialog.addEventListener('click', () => {
     form.reset();
     dialog.close();
 });
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    
+form.addEventListener('submit', () => {    
     let formData = new FormData(form);
     let formObject = Object.fromEntries(formData.entries());
     
@@ -28,9 +27,19 @@ form.addEventListener('submit', (event) => {
     displayLibrary();
     form.reset();
     dialog.close();
-    
 });
 
+libraryBoard.addEventListener('click', (event) => {    
+    if (event.target.classList.contains('btn-remove-book')) {
+        let dataID = event.target.parentNode.dataset.id;
+        let book = myLibrary.find((book) => book.id == dataID);
+
+        event.target.parentNode.remove();
+        myLibrary.splice(myLibrary.indexOf(book), 1);     
+    }
+});
+
+// CONSTRUCTORS
 function Book(title, author, pages) {
     this.title = title;
     this.author = author;
@@ -38,6 +47,7 @@ function Book(title, author, pages) {
     this.id = self.crypto.randomUUID();
 }
 
+// FUNCTIONS
 function addBookToLibrary(title, author, pages) {
     myLibrary.push(new Book(title, author, pages));
 }
@@ -45,19 +55,23 @@ function addBookToLibrary(title, author, pages) {
 function displayLibrary() {
     libraryBoard.replaceChildren();
 
-    for (const book of myLibrary) {   
+    for (let book of myLibrary) {   
         let div = document.createElement("div");
         let title = document.createElement("h3");
         let author = document.createElement("p");
         let pages = document.createElement("p");
-    
+        let btn = document.createElement("button");
+
         title.textContent = book.title;
         author.textContent = `by ${book.author}`;
         pages.textContent = `${book.pages} pages`;
+        btn.textContent = 'Remove';
     
+        div.setAttribute('data-id', book.id)
         div.classList.add("book-card");
+        btn.classList.add("btn-remove-book");
     
-        div.append(title, author, pages);
+        div.append(title, author, pages, btn);
         libraryBoard.append(div);
     }   
 }
