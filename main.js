c01cc9bebe114fe972fe5c8e95986905741c7cf4
kladov-vv <kladov.vv@yandex.com>
2025-12-30 21:37:26 +1000

    Add ability to change read status of each book in library

--- main.js
+++ main.js
@@ -3,10 +3,9 @@ const form = document.querySelector('form');
 const btnCloseDialog = document.querySelector('.btn-close-dialog');
 const btnAddBook = document.querySelector('.btn-add-book');
 const libraryBoard = document.querySelector('.library-board');
-
 const myLibrary = [
-    {title: 'book 1', author: 'author 1', pages: 100, id: 1},
-    {title: 'book 2', author: 'author 2', pages: 200, id: 2}
+    {title: 'book 1', author: 'author 1', pages: 100, id: '1'},
+    {title: 'book 2', author: 'author 2', pages: 200, id: '2'}
 ];
 
 // EVENTS
@@ -23,33 +22,44 @@ form.addEventListener('submit', () => {
     let formData = new FormData(form);
     let formObject = Object.fromEntries(formData.entries());
     
-    addBookToLibrary(formObject.title, formObject.author, formObject.pages)    
+    addBookToLibrary(formObject.title, formObject.author, formObject.pages, formObject.status)    
     displayLibrary();
     form.reset();
     dialog.close();
 });
 
 libraryBoard.addEventListener('click', (event) => {    
+    if (event.target.tagName != 'BUTTON') return;
+    
+    let dataID = event.target.parentNode.dataset.id;
+    let bookIndex = myLibrary.findIndex((book) => book.id === dataID);
+    
     if (event.target.classList.contains('btn-remove-book')) {
-        let dataID = event.target.parentNode.dataset.id;
-        let book = myLibrary.find((book) => book.id == dataID);
-
         event.target.parentNode.remove();
-        myLibrary.splice(myLibrary.indexOf(book), 1);     
+        myLibrary.splice(bookIndex, 1);
+    }
+
+    if (event.target.classList.contains('btn-change-status')) {
+        myLibrary[bookIndex].changeStatus();
+        displayLibrary();
     }
 });
 
 // CONSTRUCTORS
-function Book(title, author, pages) {
+function Book(title, author, pages, status) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.status = status;
     this.id = self.crypto.randomUUID();
+    this.changeStatus = function() {
+        this.status = (this.status === 'on') ? 'off' : 'on';
+    }
 }
 
 // FUNCTIONS
-function addBookToLibrary(title, author, pages) {
-    myLibrary.push(new Book(title, author, pages));
+function addBookToLibrary(title, author, pages, status) {
+    myLibrary.push(new Book(title, author, pages, status));
 }
 
 function displayLibrary() {
@@ -60,18 +70,23 @@ function displayLibrary() {
         let title = document.createElement("h3");
         let author = document.createElement("p");
         let pages = document.createElement("p");
-        let btn = document.createElement("button");
+        let status = document.createElement("p");
+        let btnRemove = document.createElement("button");
+        let btnChangeStatus = document.createElement("button");
 
         title.textContent = book.title;
         author.textContent = `by ${book.author}`;
         pages.textContent = `${book.pages} pages`;
-        btn.textContent = 'Remove';
+        status.textContent = (book.status === 'on') ? 'read' : 'not read';
+        btnChangeStatus.textContent = 'Change status';
+        btnRemove.textContent = 'Remove';
     
         div.setAttribute('data-id', book.id)
         div.classList.add("book-card");
-        btn.classList.add("btn-remove-book");
+        btnChangeStatus.classList.add("btn-change-status");
+        btnRemove.classList.add("btn-remove-book");
     
-        div.append(title, author, pages, btn);
+        div.append(title, author, pages, status, btnChangeStatus, btnRemove);
         libraryBoard.append(div);
     }   
 }
